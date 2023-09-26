9c7fed5bff1bcdae054b01fd69c0ff4b53ba2b1d
CG <105956982+Cg9291@users.noreply.github.com>
2023-09-25 23:12:03 -0700

    added navigation headers

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import ContainerPrototype from "./components/prototypes/ContainerPrototype.tsx";
 import styled from "styled-components";
 import { JsxElement } from "typescript";
+import Navigation from "./components/navigation/Navigation.tsx";
 
 const Container = styled(ContainerPrototype)`
-	background-color: yellow;
+	background-color: black;
+	width: 100vw;
+	height: 100vh;
 `;
 
 function App(): JSX.Element {
-	return <Container></Container>;
+	return (
+		<Container>
+			<Navigation />
+		</Container>
+	);
 }
 
 export default App;
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,16 +1,44 @@
 import styled from "styled-components";
 import ContainerPrototype from "../prototypes/ContainerPrototype.tsx";
 
+const navHeaders: string[] = ["about", "work", "contact"];
+
 const Container = styled(ContainerPrototype)`
 	height: 10rem;
+	justify-content: center;
 `;
 
-const NavList styled.ul`
-    display:flex;
-    width:30%;
+const NavList = styled.ul`
+	display: flex;
+	justify-content: center;
+	width: 30%;
+	list-style: none;
+`;
 
-`
+const ListItem = styled.li`
+	margin: 0 1rem;
+`;
+
+const ListText = styled.div`
+	color: white;
+	font-size: 1rem;
+`;
+/* const dislayListItems=():[]=>navHeaders.map(header=>) */
 
 export default function Navigation(): JSX.Element {
-	return <Container></Container>;
+	return (
+		<Container>
+			<NavList>
+				<ListItem>
+					<ListText>about</ListText>
+				</ListItem>
+				<ListItem>
+					<ListText>work</ListText>
+				</ListItem>
+				<ListItem>
+					<ListText>contact</ListText>
+				</ListItem>
+			</NavList>
+		</Container>
+	);
 }
